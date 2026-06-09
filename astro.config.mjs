6ac1be301e88c2d161b3fba50a5a004ec86d35a5
v.matveeva <vika030718@gmail.com>
2026-06-09 19:56:01 -0400

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://vika030718.github.io",
-  });
-
-
+  site: "https://vika030718.github.io",
+  base: "/nebulous-neutron",
+});
